2bdf75c36b6fba70db4ca480734ddcf6416faa9c
Alexi garrow <agarrow@getbenepass.com>
2023-12-31 12:05:12 -0500

    delete unneeded files

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,6 +12,50 @@ from django.db import models
 from django.apps.registry import apps
 
 
+@pytest.fixture
+def assert_unsafe(setup_migration_state):
+    def _factory(
+        migration_name: str,
+        info_message: Optional[str] = None,
+        pg_version: Optional[int] = None,
+    ):
+        info_message = info_message or migration_name
+        migration, pre_migration_state = setup_migration_state(
+            migration_name=migration_name
+        )
+        with pytest.raises(
+            UnsafeMigrationError, match=re.escape(INFO_MESSAGES[info_message])
+        ):
+            check_migration_safety(
+                migration=migration,
+                pg_major_version=pg_version,
+                project_state=pre_migration_state,
+            )
+
+    return _factory
+
+
+@pytest.fixture
+def assert_safe(setup_migration_state):
+    def _factory(
+        migration_name: str,
+        pg_version: Optional[int] = None,
+    ):
+        migration, pre_migration_state = setup_migration_state(
+            migration_name=migration_name
+        )
+        assert (
+            check_migration_safety(
+                migration=migration,
+                pg_major_version=pg_version,
+                project_state=pre_migration_state,
+            )
+            is None
+        )
+
+    return _factory
+
+
 @pytest.fixture()
 def test_app_path(test_path):
     return os.path.join(test_path, "test_project")
@@ -98,50 +142,6 @@ def setup_migration_state(
         pre_migration_state = make_project_state(model=user_model)
         migration = make_migration(migration_name=migration_name)
         return migration, pre_migration_state
-
-    return _factory
-
-
-@pytest.fixture
-def assert_unsafe(setup_migration_state):
-    def _factory(
-        migration_name: str,
-        info_message: Optional[str] = None,
-        pg_version: Optional[int] = None,
-    ):
-        info_message = info_message or migration_name
-        migration, pre_migration_state = setup_migration_state(
-            migration_name=migration_name
-        )
-        with pytest.raises(
-            UnsafeMigrationError, match=re.escape(INFO_MESSAGES[info_message])
-        ):
-            check_migration_safety(
-                migration=migration,
-                pg_major_version=pg_version,
-                project_state=pre_migration_state,
-            )
-
-    return _factory
-
-
-@pytest.fixture
-def assert_safe(setup_migration_state):
-    def _factory(
-        migration_name: str,
-        pg_version: Optional[int] = None,
-    ):
-        migration, pre_migration_state = setup_migration_state(
-            migration_name=migration_name
-        )
-        assert (
-            check_migration_safety(
-                migration=migration,
-                pg_major_version=pg_version,
-                project_state=pre_migration_state,
-            )
-            is None
-        )
 
     return _factory
 
--- tests/test_project/test_project/settings.py
+++ tests/test_project/test_project/settings.py
@@ -35,27 +35,6 @@ INSTALLED_APPS = ["testapp", "legacyapp", "strong_migrations"]
 
 MIDDLEWARE = []
 
-# ROOT_URLCONF = "test_project.urls"
-
-TEMPLATES = [
-    {
-        "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
-        "APP_DIRS": True,
-        "OPTIONS": {
-            "context_processors": [
-                "django.template.context_processors.debug",
-                "django.template.context_processors.request",
-                "django.contrib.auth.context_processors.auth",
-                "django.contrib.messages.context_processors.messages",
-            ],
-        },
-    },
-]
-
-WSGI_APPLICATION = "test_project.wsgi.application"
-
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
@@ -77,45 +56,6 @@ else:
             "NAME": BASE_DIR / "db.sqlite3",
         }
     }
-
-
-# Password validation
-# https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
-
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
-    },
-    {
-        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
-    },
-]
-
-
-# Internationalization
-# https://docs.djangoproject.com/en/3.2/topics/i18n/
-
-LANGUAGE_CODE = "en-us"
-
-TIME_ZONE = "UTC"
-
-USE_I18N = True
-
-USE_L10N = True
-
-USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.2/howto/static-files/
-
-STATIC_URL = "/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
